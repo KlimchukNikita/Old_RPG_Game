ebb4dfba441aab8986d9cf3b95313dc52f1d6197
Klimchuk Nikita <52635737+KlimchukNikita@users.noreply.github.com>
2020-07-12 23:11:23 +0300

    Add files via upload

---
+++ Version_0.02/Version_0.02/Old_RPG_Game.pas
@@ -0,0 +1,29 @@
+{************************************************************}
+{                                                            }
+{                    Project Old_RPG_Game                    }
+{    Copyright (c) 2020 <KlimchukNik_MasterPiece_Studio>     }
+{              https://github.com/KlimchukNikita             }
+{                                                            }
+{              Development: Klimchuk Nikita                  }
+{              Version: Stage 1. Engine. 0.02                }
+{                                                            }
+{************************************************************}
+
+{$codepage utf8}
+
+program Old_RPG_Game;
+
+uses
+
+  crt,
+  unit1,
+  unit2;
+
+begin
+
+  frame1();
+  clrscr();
+  frame2();
+
+end.
+
